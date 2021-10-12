f5c99ef2dbd78fd49fab7426b134de361aa55d82
Michal Trubini <mchl.trubini@gmail.com>
2021-10-12 08:57:01 +0200

    added js to feed json data

--- js/script.js
+++ js/script.js
@@ -1,57 +1,36 @@
-/* fetch('data.json')
-    .then(response => response.json())
-    .then(data => {
-        console.log(data[0].timeframes.weekly.current);
-        document.querySelector('#work-now').innerText = data[0].timeframes.weekly.current;
-        document.querySelector('#work-before').innerText = data[0].timeframes.weekly.previous;
-        document.querySelector('#play-now').innerText = data[1].timeframes.weekly.current;
-        document.querySelector('#play-before').innerText = data[1].timeframes.weekly.previous;
-        document.querySelector('#study-now').innerText = data[2].timeframes.weekly.current;
-        document.querySelector('#study-before').innerText = data[2].timeframes.weekly.previous;
-        document.querySelector('#exercise-now').innerText = data[3].timeframes.weekly.current;
-        document.querySelector('#exercise-before').innerText = data[3].timeframes.weekly.previous;
-        document.querySelector('#social-now').innerText = data[4].timeframes.weekly.current;
-        document.querySelector('#social-before').innerText = data[4].timeframes.weekly.previous;
-        document.querySelector('#selfcare-now').innerText = data[5].timeframes.weekly.current;
-        document.querySelector('#selfcare-before').innerText = data[5].timeframes.weekly.previous;
-    }) */
+feedData('weekly');
+document.getElementById('weekly').classList.add('selected');
 
-    const period = 'weekly';
+const link = document.querySelectorAll('.profile__period-switcher-item');
 
-    fetch('data.json')
-    .then(response => response.json())
-    .then(data => {
-        console.log(data[0].timeframes[period].current);
-        document.querySelector('#work-now').innerText = data[0].timeframes[period].current;
-        document.querySelector('#work-before').innerText = data[0].timeframes[period].previous;
-        document.querySelector('#play-now').innerText = data[1].timeframes[period].current;
-        document.querySelector('#play-before').innerText = data[1].timeframes[period].previous;
-        document.querySelector('#study-now').innerText = data[2].timeframes[period].current;
-        document.querySelector('#study-before').innerText = data[2].timeframes[period].previous;
-        document.querySelector('#exercise-now').innerText = data[3].timeframes[period].current;
-        document.querySelector('#exercise-before').innerText = data[3].timeframes[period].previous;
-        document.querySelector('#social-now').innerText = data[4].timeframes[period].current;
-        document.querySelector('#social-before').innerText = data[4].timeframes[period].previous;
-        document.querySelector('#selfcare-now').innerText = data[5].timeframes[period].current;
-        document.querySelector('#selfcare-before').innerText = data[5].timeframes[period].previous;
+link.forEach(e => e.addEventListener('click', function (event) {
+    
+    link.forEach(e => e.classList.remove('selected'));
+
+    this.classList.add('selected');
+    const period = this.id;
+
+    feedData(period);
+
+    event.preventDefault();
     })
+);
 
-function getData (period){
+function feedData(period) {
     fetch('data.json')
-    .then(response => response.json())
-    .then(data => {
-        console.log(data[0].timeframes['period'].current);
-        document.querySelector('#work-now').innerText = data[0].timeframes['period'].current;
-        document.querySelector('#work-before').innerText = data[0].timeframes['period'].previous;
-        document.querySelector('#play-now').innerText = data[1].timeframes['period'].current;
-        document.querySelector('#play-before').innerText = data[1].timeframes['period'].previous;
-        document.querySelector('#study-now').innerText = data[2].timeframes['period'].current;
-        document.querySelector('#study-before').innerText = data[2].timeframes['period'].previous;
-        document.querySelector('#exercise-now').innerText = data[3].timeframes['period'].current;
-        document.querySelector('#exercise-before').innerText = data[3].timeframes['period'].previous;
-        document.querySelector('#social-now').innerText = data[4].timeframes['period'].current;
-        document.querySelector('#social-before').innerText = data[4].timeframes['period'].previous;
-        document.querySelector('#selfcare-now').innerText = data[5].timeframes['period'].current;
-        document.querySelector('#selfcare-before').innerText = data[5].timeframes['period'].previous;
+        .then(response => response.json())
+        .then(data => {
+            document.querySelector('#work-now').innerText = data[0].timeframes[period].current;
+            document.querySelector('#work-before').innerText = data[0].timeframes[period].previous;
+            document.querySelector('#play-now').innerText = data[1].timeframes[period].current;
+            document.querySelector('#play-before').innerText = data[1].timeframes[period].previous;
+            document.querySelector('#study-now').innerText = data[2].timeframes[period].current;
+            document.querySelector('#study-before').innerText = data[2].timeframes[period].previous;
+            document.querySelector('#exercise-now').innerText = data[3].timeframes[period].current;
+            document.querySelector('#exercise-before').innerText = data[3].timeframes[period].previous;
+            document.querySelector('#social-now').innerText = data[4].timeframes[period].current;
+            document.querySelector('#social-before').innerText = data[4].timeframes[period].previous;
+            document.querySelector('#selfcare-now').innerText = data[5].timeframes[period].current;
+            document.querySelector('#selfcare-before').innerText = data[5].timeframes[period].previous;
     })
-};
+}
